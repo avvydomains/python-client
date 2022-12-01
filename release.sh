b92b57732acf70fe1755fec02cbfe8b7024d66a8
Connor Bode <connor.bode@gmail.com>
2022-12-01 01:17:26 -0500

    v0.1.0

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+AVVY_CLIENT_COMMON_TMP=$AVVY_CLIENT_COMMON
+unset AVVY_CLIENT_COMMON
+python3 build.py
+
 echo "Current version is $(cat VERSION.txt).  What version did you want to release?"
 read VERSION
 echo $VERSION > VERSION.txt
@@ -7,3 +11,4 @@ twine upload ".tar.gz"
 git add .
 git commit -m "v$VERSION"
 git tag -a "v$VERSION" -m "v$VERSION"
+
